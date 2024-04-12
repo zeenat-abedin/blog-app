caac47359d8ac1fefbc2ec2bd51dc1c59c5332cf
zeenat <zeenu.23@gmail.com>
2024-04-12 13:36:03 +0530

    import and wrap Signup component in Base component

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import Base from '../components/Base'
 
 function Signup() {
-  return (
-    <div>
+    return (
+    <Base>
+      <div>
       <h1>This is a Signup page</h1>
       <p>Welocme to the Signup page</p>
-    </div>
+      </div>
+    </Base>
   )
 }
 
