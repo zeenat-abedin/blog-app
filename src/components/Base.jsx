80d57abd6558d0d1b5c1205e3e7cb237886f109e
zeenat <zeenu.23@gmail.com>
2024-04-12 13:31:05 +0530

    added 'p-0 m-0' classes to container-fluid div in Base component

--- src/components/Base.jsx
+++ src/components/Base.jsx
@@ -3,7 +3,7 @@ import CustomNavbar from './CustomNavbar'
 
 function Base({children}) {
   return (
-      <div className="container-fluid">
+      <div className="container-fluid p-0 m-0">
          <CustomNavbar/>
           {children}
           <h1>This is a footer</h1>
